9437846f4e6aa34e8c1ba31d2e2faa05f595e81d
Faraon-ua <faraon.ua@gmail.com>
2017-06-22 16:29:53 +0300

    facebook webhook 3

---
+++ Benefit.Web/Controllers/API/FacebookMessengerController.cs
@@ -0,0 +1,18 @@
+﻿using System.Net;
+using System.Web.Http;
+using Benefit.Services;
+
+namespace Benefit.Web.Controllers.API
+{
+    public class FacebookMessengerController : ApiController
+    {
+        public IHttpActionResult Get([FromUri(Name = "hub.mode")] string mode, [FromUri(Name = "hub.challenge")] string challenge, [FromUri(Name = "hub.verify_token")] string verifyToken)
+        {
+            if (mode == "subscribe" && verifyToken == SettingsService.Notifications.FacebookVerificationToken)
+            {
+                return Ok(challenge);
+            }
+            return StatusCode(HttpStatusCode.Forbidden);
+        }
+    }
+}
