be74a4991cbd6a85936b5c6c9683028dd76771db
Faraon-ua <faraon.ua@gmail.com>
2017-12-11 15:31:30 +0200

    fix

--- Benefit.CardReader/Controls/Loading.xaml.cs
+++ Benefit.CardReader/Controls/Loading.xaml.cs
@@ -1,16 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Windows;
+﻿using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace Benefit.CardReader.Controls
 {
@@ -22,6 +11,11 @@ namespace Benefit.CardReader.Controls
         public Loading()
         {
             InitializeComponent();
+        }
+
+        private void Loading_OnIsVisibleChanged(object sender, DependencyPropertyChangedEventArgs e)
+        {
+            LoadImg.Spin = IsVisible;
         }
     }
 }
