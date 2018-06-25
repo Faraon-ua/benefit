06b289c29baf740fcdd92afb921f686013f59fa3
Faraon-ua <faraon.ua@gmail.com>
2018-06-25 15:23:15 +0300

    cart fix

---
+++ Benefit.Web/Controllers/SellerController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Benefit.Domain.DataAccess;
+
+namespace Benefit.Web.Controllers
+{
+    public class SellerController : Controller
+    {
+        private ApplicationDbContext db = new ApplicationDbContext();
+
+        public ActionResult Index()
+        { //var seller = db.Sellers
+            //    .Include(entry => entry.SellerCategories.Select(sc => sc.Category))
+            //    .FirstOrDefault(entry => entry.UrlName == id);
+            //if (seller == null) return new HttpNotFoundResult();
+            //var categories = seller.SellerCategories.Select(entry => entry.Category).ToList().SortByHierarchy().ToList();
+            //var products = db.pro
+            return View();
+        }
+    }
+}
