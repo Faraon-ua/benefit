ca5f51309c7a7cc81bc2da226d404b94fd7dffc1
Faraon-ua <faraon.ua@gmail.com>
2019-03-19 16:42:40 +0200

    variants fix

--- Benefit.Web/Scripts/v3/products.js
+++ Benefit.Web/Scripts/v3/products.js
@@ -1,28 +1,28 @@
-﻿$(function() {
+﻿$(function () {
     function CalculateProductPrice() {
-        var checkOptions = $(".product_modal_form input[type=checkbox]:checked").map(function() {
+        var checkOptions = $(".product_modal_form input[type=checkbox]:checked").map(function () {
             var checkboxPriceGrowth = parseFloat($(this).attr("data-price-growth"));
             var amount = $(this).siblings(".counter").find(".quantity").val();
             return checkboxPriceGrowth * amount;
         }).get();
-        var radioOptions = $(".product_modal_form input[type=radio]:checked").map(function() {
+        var radioOptions = $(".product_modal_form input[type=radio]:checked").map(function () {
             return parseFloat($(this).attr("data-price-growth"));
         }).get();
-        var checkPriceGrowth = checkOptions.reduce(function(pv, cv) { return pv + cv; }, 0);
-        var radioPriceGrowth = radioOptions.reduce(function(pv, cv) { return pv + cv; }, 0);
+        var checkPriceGrowth = checkOptions.reduce(function (pv, cv) { return pv + cv; }, 0);
+        var radioPriceGrowth = radioOptions.reduce(function (pv, cv) { return pv + cv; }, 0);
         var originalPrice = parseFloat($(".product-price").attr("data-original-price"));
         $(".product-price").text((originalPrice + checkPriceGrowth + radioPriceGrowth).toFixed(2));
     }
 
     $('body').on("blur",
         ".product_modal_form .counter",
-        function() {
+        function () {
             CalculateCartSum();
         });
 
     $("body").on("click",
         ".product_modal_form input[type=checkbox], .product_modal_form input[type=radio]",
-        function() {
+        function () {
             var id = $(this).attr("id");
             $(".product_modal_form input[data-binded-option-id=" + id + "]").prop('checked', true);
             CalculateProductPrice();
@@ -30,13 +30,13 @@
 
     $("body").on("click",
         ".product_modal_form div.minus, .product_modal_form div.plus",
-        function() {
+        function () {
             CalculateProductPrice();
         });
 
     $("body").on('click',
         ".product_modal_form .plus, .product_modal_form .minus, .product-item .plus, .product-item .minus, .product-page .plus, .product-page .minus",
-        function() {
+        function () {
             var quantity = $(this).parent().find('input[name=quantity]');
             var valueToAdd = 1;
             var isMinus = $(this).hasClass("minus");
@@ -59,7 +59,7 @@
 
     $("body").on('click',
         "#buy-product-with-options",
-        function() {
+        function () {
             var productId = $(this).attr("data-product-id");
             var sellerId = $(this).attr("data-seller-id");
             AddOrderProduct(1, productId, sellerId, true, false);
@@ -68,7 +68,7 @@
     //variants
     $("body").on("click",
         ".variants .variant-item",
-        function() {
+        function () {
             $(this).siblings().removeClass("active");
             $(this).addClass("active");
         });
@@ -76,36 +76,41 @@
 
     $("body").on('click',
         "#buy-product-with-variants",
-        function () {
+        function() {
             var productNameSufix = "";
             var productPriceGrowth = 0;
             var productId = $(this).attr("data-product-id");
             var sellerId = $(this).attr("data-seller-id");
+            var isValid = true;
             var variantsWrap = $(this).parentsUntil(".variants");
-            var selectedVars = variantsWrap.find(".variant-item.active");
-            selectedVars.each(function () {
-                productNameSufix += " " +
-                    $(this).attr("data-option-title") +
-                    " " +
-                    $(this).attr("data-option-name");
-                productPriceGrowth += parseFloat($(this).attr("data-price-growth"));
+            variantsWrap.find(".variants-group").each(function() {
+                var selectedVariant = $(this).find(".variant-item.active");
+                if (selectedVariant.length === 0) {
+                    $(this).find(".validation").show();
+                    isValid = false;
+                } else {
+                    productNameSufix += " " +
+                        selectedVariant.attr("data-option-title") +
+                        " " +
+                        selectedVariant.attr("data-option-name");
+                    productPriceGrowth += parseFloat(selectedVariant.attr("data-price-growth"));
+                }
             });
-            if (selectedVars.length == 0) {
-                $(this).parents(".variants").find(".validation").show();
-            } else {
+            if (isValid) {
                 AddOrderProduct(1,
                     productId,
                     sellerId,
                     true,
                     false,
-                   productNameSufix, productPriceGrowth);
+                    productNameSufix,
+                    productPriceGrowth);
                 $(".modal.variants").modal('hide');
             }
         });
 
     $("body").on("click",
         ".product_buy",
-        function(e) {
+        function (e) {
             e.preventDefault();
             if ($(this).attr("disabled")) {
                 return;
@@ -118,13 +123,13 @@
                 amount = 1;
             }
             $.get(productOptionsUrl + "?productId=" + productId,
-                function(data) {
+                function (data) {
                     if (data) {
                         $("#product-options-wrap").html(data);
                         $("#product_modal").modal('show');
                     } else {
                         $.get(productVariantsUrl + "?productId=" + productId,
-                            function(data) {
+                            function (data) {
                                 if (data) {
                                     $("#product-variants-wrap").html(data);
                                     $(".variants.modal").modal('show');
