dc3bbbdc7e6f9be1f2322d4909600a8abcd63ed0
Faraon-ua <faraon.ua@gmail.com>
2019-06-01 22:51:37 +0300

    fix

--- Benefit.Services/Domain/SellerService.cs
+++ Benefit.Services/Domain/SellerService.cs
@@ -224,7 +224,7 @@ namespace Benefit.Services.Domain
                 Name = "Каталог постачальників",
                 ChildAsFilters = true,
                 //ChildCategories = db.Categories.Where(entry => entry.ParentCategoryId == null && entry.IsActive && !entry.IsSellerCategory).ToList().MapToVM(),
-                ChildCategories = db.Sellers.Where(entry => entry.CategoryName != null).Select(entry => new CategoryVM() { Name = entry.CategoryName }).ToList(),
+                ChildCategories = db.Sellers.Where(entry => entry.CategoryName != null).Select(entry=>entry.CategoryName).Distinct().Select(entry => new CategoryVM() { Name = entry }).ToList(),
             };
             return result;
         }
