325548d118f74244338130cdb31f70c41d5281ce
Faraon-ua <faraon.ua@gmail.com>
2020-07-20 15:47:36 +0300

    bug fix

--- Benefit.Web/Areas/Admin/Controllers/BalanceController.cs
+++ Benefit.Web/Areas/Admin/Controllers/BalanceController.cs
@@ -83,7 +83,7 @@ namespace Benefit.Web.Areas.Admin.Controllers
                 Pages = transactionsTotal / ListConstants.AdminTakePerPage + 1,
                 ActivePage = page
             };
-            model.SellerReports = db.SellerReports.Where(entry => entry.SellerId == Seller.CurrentAuthorizedSellerId).OrderByDescending(entry => entry.Date).ToList();
+            model.SellerReports = db.SellerReports.Where(entry => entry.SellerId == Seller.CurrentAuthorizedSellerId).OrderBy(entry => entry.Year).ThenBy(entry=>entry.Month).ToList();
             return View(model);
         }
         // GET: Admin/Balance
